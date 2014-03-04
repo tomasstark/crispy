907c46c209b3f4ffb3c6236a8fa783f5858f70ee
Tomas Stark <tom@tomstark.me>
2014-03-04 21:53:37 +0100

    image sizes

--- functions/content/image-sizes.php
+++ functions/content/image-sizes.php
@@ -5,7 +5,8 @@
 
     function set_custom_image_sizes() {
         // square thumbnail
-        add_image_size( 'square', 300, 300, true );
+        add_image_size( 'center-img', 720, 0, false );
+        add_image_size( 'thumb-img', 300, 0, false );
     }
 
     add_action( 'after_setup_theme', 'set_custom_image_sizes' );
